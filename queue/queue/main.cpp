38bcc3a6510371368f9b0db105c8181608c2cb85
BorisVikVik <boris@victorov.com>
2020-04-08 23:39:25 +0300

    Queue task added

--- queue/queue/main.cpp
+++ queue/queue/main.cpp
@@ -19,9 +19,11 @@ struct queue
 		int pointerTop, pointerBot;
 		bool start;
 		bool end;
+		bool f;
 		queue(int size = SIZE)
 		{
 			start = 0;
+			f = 0;
 			end = 0;
 			arr = new int[size]; 
 			pointerTop = -1;//присваеваем индексу добавляемого элемента значение 
@@ -37,15 +39,18 @@ struct queue
 			{
 				pointerTop = -1;
 				end = 1;
+				//cout <<"gay"<< end << endl;
 			}
-			if ((pointerTop+1 == pointerBot) && start != 0)
+			if ((pointerTop+1 == pointerBot) && start != 0 && f != 1)
 			{
+				
 				return 0;
 			}
 			else 
 			{
 				arr[++pointerTop] = x;
 				start = 1;
+				f = 0;
 			}
 		}
 		/*int pop()
@@ -61,19 +66,34 @@ struct queue
 		}*/
 		int pop()
 		{
-			if (pointerBot == SIZE)
-			{
-				pointerBot = 0;
-				end = 0;
-			}
-			//cout <<"s "<< start <<" B "<<pointerBot << "T" << pointerTop << endl;
+			
+			//cout <<"E "<< end <<" B "<<pointerBot << "T" << pointerTop << endl;
 			if ((pointerBot-1 == pointerTop) && end != 1)
 			{
+				f = 1;
+				//cout << "YAY";
 				return 0;
-				
 			}
 			else
 			{
+				if ((pointerBot == pointerTop) && end != 1)
+				{
+					f = 1;
+					//return 0;
+				}
+				if (pointerBot+1 == SIZE)
+				{
+					int tmp = arr[pointerBot];
+					pointerBot = 0;
+					end = 0;
+					if ((pointerBot-1 == pointerTop))
+					{
+						f = 1;
+						//cout << "OMG";
+						//return 0;
+					}
+					return tmp;
+				}
 				return arr[pointerBot++];
 			}
 			//if (pointerBot == SIZE)
@@ -124,27 +144,57 @@ int main()
 	bruh.push(2);
 	bruh.push(3);
 	bruh.push(4);
-	cout << bruh.pointerTop << endl;
+	//cout << bruh.pointerTop << endl;
 	bruh.push(5);
 	bruh.push(6);
 	bruh.push(7);
 	cout << bruh.front() << endl;
 	cout << bruh.back() << endl;
 	cout << bruh.pop() << endl;
-	cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerBot << endl;
 	cout << bruh.pop() << endl;
-	cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerBot << endl;
 	cout << bruh.pop() << endl;
+	
+	//cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerTop << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
 	cout << endl;
-	cout << bruh.pointerBot << endl;
-	cout << bruh.pointerTop << endl;
+	bruh.push(1);
+	bruh.push(2);
+	bruh.push(3);
+	bruh.push(4);
+	//cout << bruh.pointerTop << endl;
+	bruh.push(5);
+	bruh.push(6);
+	//cout << bruh.pointerTop << endl;
+	bruh.push(7);
+	//cout << bruh.pointerTop << endl;
+	cout << bruh.front() << endl;
+	cout << bruh.back() << endl;
 	cout << bruh.pop() << endl;
-	cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerBot << endl;
 	cout << bruh.pop() << endl;
-	cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerBot << endl;
 	cout << bruh.pop() << endl;
-	cout << bruh.pointerBot << endl;
+	//cout << endl;
+	//cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerTop << endl;
 	cout << bruh.pop() << endl;
-	cout << bruh.pointerBot << endl;
+	//cout << bruh.pointerBot << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
+	cout << bruh.pop() << endl;
+	//cout << bruh.pointerBot << endl;
+
 	return 0;
 }
