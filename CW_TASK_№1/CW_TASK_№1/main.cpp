3099731bbfbc5dca6a784a7cdaa4ad5100a3ed3e
BorisVikVik <boris@victorov.com>
2020-04-29 09:44:21 +0300

    Added comments to task 1

--- CW_TASK_№1/CW_TASK_№1/main.cpp
+++ CW_TASK_№1/CW_TASK_№1/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 vector<int> arr(0);
 
-void printD(int number)
+void printD(int number)//function that prints array that is a decompiled number
 {
 	int sum41 = 0;
 	for (int t = 0; t < number; t++)
 	{
-		if (sum41 + arr[t] == number)
+		if (sum41 + arr[t] == number) // if the our summary plus next element of array will give our number, we print it and stop the printing of array
 		{
 			cout << arr[t];
 			cout << endl;
@@ -20,25 +20,31 @@ void printD(int number)
 		sum41 += arr[t];
 	}
 }
-
-void decomp(int num, int first, int last, int sum)
+/*function that decompiles our number
+Logic: It uses recursion to change next element of array. It enumirates element, than check that our summary isn't bigger then our number, if it isn't then we go to next element and check 
+our summary. If it equal then we print it with function printD and then go back to previous element. If it not equal to our number we start enumiration of next element and the operation 
+repeats. If we enumirated all the options of current element than we go back to previous element. If we went to first element and enumirated it then we print our number that is our last 
+decomposition. To escape the repeating of the number we start the enumiration of the next element with the previous element.
+arg: num - is our number, element - is the number of our current element, last - is previous element, sum - is our current summary.
+*/
+void decomp(int num, int element, int last, int sum)
 {
-	if (sum == num)
+	if (sum == num)//The end of recursion
 	{
-		printD(num);
+		printD(num);//printing the line
 		return;
 	}
-	for (int i = last; first < num && i < num; i++)
+	for (int i = last; i < num; i++) // it goes until it check all the options
 	{
-		arr[first] = i;
-		if (sum + i > num)break;
-		decomp(num, first + 1, i, sum + i);
+		arr[element] = i;// changing the current element with current option
+		if (sum + i > num)break; //check that our summary isn't bigger then our number.
+		decomp(num, element + 1, i, sum + i); // recursion
 	}
-	if (first == 0)
+	if (element == 0)//if it a first element and the enumiration ended then it means that we checked all the options
 	{
-		cout << num;
+		cout << num;//so we print our number that is our final decompilation
 	}
-	return;
+	return;//Second end of recursion
 }
 
 int main()
